15b94ab97f95198d26948549a431ba102b51e7e1
Arvid Hossain <arvidh97@gmail.com>
2023-05-19 15:13:37 -0400

    attempted onmouse enter on edit

--- practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
+++ practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
@@ -3,9 +3,6 @@ import {useState} from 'react'
 const PawsRatingInput = ({ rating, disabled, onChange }) => {
   const [activeRating, setActiveRating]=useState(0)
 
-  const onMouseEnter = (e) => {
-    setActiveRating = e.target
-  }
   return (
     <>
     <input
@@ -17,27 +14,27 @@ const PawsRatingInput = ({ rating, disabled, onChange }) => {
     <div className="rating-input">
       <div className={activeRating >= 1 ? "filled" : "empty"}
       onMouseEnter={()=>setActiveRating(1)}
-      onMouseLeave={()=>setActiveRating(0)}>
+      onMouseLeave={()=>setActiveRating(rating)}>
         <i className="fa fa-paw"></i>
       </div>
       <div className={activeRating >= 2 ? "filled" : "empty"} 
       onMouseEnter={()=>setActiveRating(2)}
-      onMouseLeave={()=>setActiveRating(0)}>
+      onMouseLeave={()=>setActiveRating(rating)}>
         <i className="fa fa-paw"></i>
       </div>
       <div className={activeRating >= 3 ? "filled" : "empty"}
       onMouseEnter={()=>setActiveRating(3)}
-      onMouseLeave={()=>setActiveRating(0)} >
+      onMouseLeave={()=>setActiveRating(rating)}>
         <i className="fa fa-paw"></i>
       </div>
       <div className={activeRating >= 4 ? "filled" : "empty"}
       onMouseEnter={()=>setActiveRating(4)}
-      onMouseLeave={()=>setActiveRating(0)}>
+      onMouseLeave={()=>setActiveRating(rating)}>
         <i className="fa fa-paw"></i>
       </div>
       <div className={activeRating >= 5 ? "filled" : "empty"}
       onMouseEnter={()=>setActiveRating(5)}
-      onMouseLeave={()=>setActiveRating(0)}>
+      onMouseLeave={()=>setActiveRating(rating)}>
         <i className="fa fa-paw"></i>
       </div>
     </div>
